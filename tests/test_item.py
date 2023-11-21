08d1d9ae83a7e6bcf3f4805ec55899002a318410
Ayrat Fatykhov <airat_91_91@mail.ru>
2023-11-21 20:15:58 +0300

    add tests for class Item

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item = Item("Процессор", 10000, 2)
+    assert item.calculate_total_price() == 20000
+
+
+def test_apply_discount():
+    item = Item("Наушники", 5000, 7)
+    Item.pay_rate = 0.5
+    item.apply_discount()
+    assert item.price == 2500
