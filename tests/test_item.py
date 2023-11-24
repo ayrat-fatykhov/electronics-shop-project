0712f52c078f22a60c311cff94f832b3a69d71e2
Ayrat Fatykhov <airat_91_91@mail.ru>
2023-11-24 20:35:53 +0300

    add tests for new foo

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from settings import ITEMS_PATH
 from src.item import Item
+
+
+def test_instance_storage():
+    item = Item("Оперативка", 2000, 5)
+    assert Item.all == [item]
 
 
 def test_calculate_total_price():
@@ -12,3 +18,14 @@ def test_apply_discount():
     Item.pay_rate = 0.5
     item.apply_discount()
     assert item.price == 2500
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv(ITEMS_PATH)
+    assert Item.all[0].name == "Смартфон"
+    assert Item.all[0].price == 100.0
+    assert Item.all[0].quantity == 1
+
+
+def test_string_to_number():
+    assert Item.string_to_number('3') == 3
