e702b638e3764ce8ca340b8253e52c0a866f996c
Ayrat Fatykhov <airat_91_91@mail.ru>
2023-12-14 23:04:02 +0300

    add links for damage file and not file

--- settings.py
+++ settings.py
@@ -3,3 +3,7 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 DATA_PATH = Path.joinpath(ROOT_PATH, "src")
 ITEMS_PATH = Path.joinpath(DATA_PATH, "items.csv")
+# Ссылка для проверки исключения при отсутствия файла
+ITEMS_NO_FILE_PATH = Path.joinpath(DATA_PATH, "item.csv")
+# Ссылка для проверки исключения при поврежденном файле
+ITEMS_NOT_COLON_PATH = Path.joinpath(DATA_PATH, "items_not_one_colon.csv")
