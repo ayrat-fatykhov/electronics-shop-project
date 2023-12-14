fee834e69b076571cc348f562df629705e0f1bbd
Ayrat Fatykhov <airat_91_91@mail.ru>
2023-12-14 23:01:07 +0300

    add links for test exceptions

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,10 +1,11 @@
+from settings import ITEMS_NOT_COLON_PATH, ITEMS_NO_FILE_PATH
 from src.item import Item
 
 if __name__ == '__main__':
     # Файл items.csv отсутствует.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv(ITEMS_NO_FILE_PATH)
     # FileNotFoundError: Отсутствует файл item.csv
 
     # В файле items.csv удалена последняя колонка.
-    Item.instantiate_from_csv()
+    Item.instantiate_from_csv(ITEMS_NOT_COLON_PATH)
     # InstantiateCSVError: Файл item.csv поврежден
