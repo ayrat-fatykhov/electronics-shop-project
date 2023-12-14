3f5fd8e22343e3e64444b67e3652be9d00f06599
Ayrat Fatykhov <airat_91_91@mail.ru>
2023-12-14 23:05:45 +0300

    add test on sim at float meaning

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -1,6 +1,17 @@
+import pytest
+
 from src.phone import Phone
 
 
 def test_repr():
     phone = Phone("Pixel 8", 100_000, 10, 1)
     assert repr(phone) == "Phone('Pixel 8', 100000, 10, 1)"
+
+
+def test_sim_quantity_negative():
+    """
+    Тест проверяет, возвращается ли ошибка при не корректно переданных данных.
+    """
+    phone = Phone("Pixel 8", 100_000, 10, 1)
+    with pytest.raises(ValueError) as err:
+        phone.sim_quantity = 0.1
